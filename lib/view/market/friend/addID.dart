8595d761167b383795fb54d8dba30bb00648ccd7
pig <21800568@handong.edu>
2021-06-29 16:57:01 +0900

    jh: search

--- lib/view/market/friend/addID.dart
+++ lib/view/market/friend/addID.dart
@@ -1,26 +1,90 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class AddIDPage extends StatefulWidget {
+  @override
+  _AddIDPageState createState() => _AddIDPageState();
+}
 
-class AddIDPage extends StatelessWidget {
+class _AddIDPageState extends State<AddIDPage> {
+  FirebaseFirestore mango_dev = FirebaseFirestore.instance;
+
+  late TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: '');
+  }
+
   @override
   Widget build(BuildContext context) {
+    String _search = '';
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ID로 추가하기'),
-      ),
-      body: SizedBox.expand(
-        // child: SafeArea(
-        //   minimum: EdgeInsets.all(0.1), child: SearchBar<>(
-        //   cancellationWidget: Text('취소'),
-        //   searchBarPadding: EdgeInsets.symmetric(horizontal: 10),
-        //   searchBarStyle: SearchBarStyle(padding: EdgeInsets.zero),
-        //   placeHolder: ListView(),
-        //
-        // ),
-        //
-        // ),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('ID로 추가하기'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CupertinoSearchTextField(
+                  controller: _textController,
+                  onChanged: (String value) {
+                    print('$value!!');
+                  },
+                  onSubmitted: (String value) {
+                    _search = value;
+                    print('result: $_search');
+                  },
+                  placeholder: '친구 검색',
+                ),
+              ),
+              Flexible(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: mango_dev.collection('temp_user').snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) return const Text('loading');
+                    return ListView.builder(
+                      itemCount: snapshot.data!.size,
+                      itemExtent: 80.0,
+                      itemBuilder: (context, index) {
+                        List<DocumentSnapshot> documents = snapshot.data!.docs;
+                        documents
+                            .map((docs) =>
+                                _buildListTile(context, docs, _search))
+                            .toList();
+                        return _buildListTile(
+                            context, documents.elementAt(index), _search);
+                      },
+                    );
+                  },
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    print('HI');
+                  },
+                  icon: Icon(Icons.add))
+            ],
+          ),
+        ));
+  }
+
+  Widget _buildListTile(
+      BuildContext context, DocumentSnapshot docs, String _search) {
+    print('Tile: $_search');
+    if (_search == docs.id) {
+      return ListTile(
+        title: Text(docs.get('name')),
+        leading: Icon(Icons.ac_unit),
+        // onTap: () {},
+      );
+    } else {
+      return SizedBox(height: 0);
+    }
   }
 }
--- lib/view/market/friend/friendList.dart
+++ lib/view/market/friend/friendList.dart
@@ -20,6 +20,12 @@ class _FriendListPageState extends State<FriendListPage> {
           title: Text('친구 목록'),
           actions: [
             IconButton(
+              onPressed: () {
+                print('HI');
+              },
+              icon: Icon(Icons.people),
+            ),
+            IconButton(
                 onPressed: () {
                   Get.bottomSheet(
                       Container(
@@ -64,27 +70,39 @@ class _FriendListPageState extends State<FriendListPage> {
           ],
           centerTitle: true,
         ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: mango_dev.collection('temp_user').snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return const Text('loading');
-            return ListView.builder(
-              itemCount: snapshot.data!.size,
-              itemExtent: 80.0,
-              itemBuilder: (context, index) {
-                List<DocumentSnapshot> documents = snapshot.data!.docs;
-                documents.map((docs) => _buildListTile(context, docs)).toList();
-                return _buildListTile(context, documents.elementAt(index));
-              },
-            );
+        body: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          stream: mango_dev.collection('temp_user').doc('123').snapshots(),
+          builder: (_, snapshot) {
+            if (snapshot.hasError) return Text('Error = ${snapshot.error}');
+
+            if (snapshot.hasData) {
+              var output = snapshot.data!.data();
+              var value = output!['FriendList']; // <-- Your value
+              return Text('Value = $value');
+            }
+
+            return Center(child: CircularProgressIndicator());
           },
-        ));
+        )
+    );
   }
 
   Widget _buildListTile(BuildContext context, DocumentSnapshot docs) {
-    // return SizedBox(height: 0);
     return ListTile(
       title: Text(docs.get('name')),
+    );
+  }
+
+  Widget _buildFriendList(DocumentSnapshot docs) {
+    // String _refID;
+    //   mango_dev.collection('temp_user').doc('123').get().then((doc) {
+    //     print(doc['refID']);
+    //     _name = doc['refID'];
+    //
+    //   });
+
+    return ListTile(
+      title: Text(docs.get('refID')),
     );
   }
 }
