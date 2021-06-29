24d3a8c272ede466ab91f8a5ba06e3daccac0899
inseokLee <tjrkd222@gmail.com>
2021-06-29 21:46:21 +0900

    6.28 - is: add chips

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ A new Flutter project.
     - 안드로이드 에뮬레이터 인터넷 연결 확인
     - Androidmanifest permission 넣어주기 (ACCESS NETWORK / INTERNET)
 
-
-
-
 - small API
     - MangoAppBar
            required String title,
            List<Widget> actions,
            required bool isLeading // whether having pop-button or not.
+
+- TODO
+    - Chip
+        하나의 칩에 하나의 Food 모델 사용.
+        Default chip 만들어 놓기.
--- lib/view/refrigerator/addFoodDirect.dart
+++ lib/view/refrigerator/addFoodDirect.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mangodevelopment/color.dart';
+import 'package:mangodevelopment/view/splash.dart';
 import 'package:mangodevelopment/view/widget/appBar.dart';
+import 'package:mangodevelopment/view/widget/chip.dart';
 
 class AddFoodDirectPage extends StatefulWidget {
   final String title;
@@ -13,10 +16,41 @@ class _AddFoodDirectPageState extends State<AddFoodDirectPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MangoWhite,
       appBar: MangoAppBar(
         isLeading: true,
         key: widget.key,
         title: widget.title,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 62 * (deviceHeight / prototypeHeight),
+            child: Row(
+              children: [
+                Container(
+                  padding:
+                      EdgeInsets.all(10 * (deviceHeight / prototypeHeight)),
+                  child: Chip(
+                    backgroundColor: Orange50,
+                    label: Icon(
+                      Icons.add,
+                      color: Orange400,
+                      size: 16.0,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [MangoChip(name: '바나나'), MangoChip(name: '바나나 우유')],
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 7 * (deviceHeight / prototypeHeight),
+            color: MangoBehindColor,
+          )
+        ],
       ),
     );
   }
