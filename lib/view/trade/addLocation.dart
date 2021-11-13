25a5fc05bc40c77a2e903f10abce6fb429ad948d
pig <21800568@handong.edu>
2021-11-13 17:57:55 +0900

    print location

--- lib/view/trade/addLocation.dart
+++ lib/view/trade/addLocation.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:mangodevelopment/view/trade/saveLocation.dart';
@@ -14,28 +15,33 @@ class addLocationPage extends StatefulWidget {
 
 class _addLocationPageState extends State<addLocationPage> {
   UserViewModel userViewModelController = Get.find<UserViewModel>();
+  late List<Placemark> placemarks;
+  GeoPoint location = GeoPoint(0, 0);
   late Position deviceLat;
   late LatLng _lastMapPosition =
       LatLng(deviceLat.latitude, deviceLat.longitude);
 
-  late Set<Marker> _marker = {
-    Marker(
-      markerId: MarkerId('marker'),
-      position: LatLng(36, 127),
-      icon: BitmapDescriptor.defaultMarker,
-    )
-  };
+  // late Set<Marker> _marker = {
+  //   Marker(
+  //     markerId: MarkerId('marker'),
+  //     position: LatLng(36, 127),
+  //     icon: BitmapDescriptor.defaultMarker,
+  //   )
+  // };
 
   @override
   void initState() {
     super.initState();
     getCurrentLocation().then((value) {
-      setState(() {
-        _marker.add(Marker(
-          markerId: MarkerId('marker'),
-          position: LatLng(deviceLat.latitude, deviceLat.longitude),
-          icon: BitmapDescriptor.defaultMarker,
-        ));
+      setState(() async {
+        placemarks = await placemarkFromCoordinates(
+            deviceLat.latitude, deviceLat.longitude);
+        // _marker.add(Marker(
+        //   markerId: MarkerId('marker'),
+        //   position: LatLng(deviceLat.latitude, deviceLat.longitude),
+        //   icon: BitmapDescriptor.defaultMarker,
+        //   draggable: true,
+        // ));
       });
     });
   }
@@ -49,8 +55,19 @@ class _addLocationPageState extends State<addLocationPage> {
   //   icon: BitmapDescriptor.defaultMarker,
   // );
 
-  void _onCameraMove(CameraPosition position) {
-    _lastMapPosition = position.target;
+  void _updatePosition(CameraPosition _position) {
+    print(_position.target.latitude);
+    // _lastMapPosition = position.target;
+
+    LatLng newMarkerPosition =
+        LatLng(_position.target.latitude, _position.target.longitude);
+    //   Marker marker = markers["marker"];
+    //
+    // setState(() {
+    //   markers["marker"] = marker.copyWith(
+    //       positionParam:
+    //           LatLng(newMarkerPosition.latitude, newMarkerPosition.longitude));
+    // });
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -73,24 +90,65 @@ class _addLocationPageState extends State<addLocationPage> {
               zoom: 16.0,
             ),
             mapType: MapType.normal,
-            markers: _marker,
-            onCameraMove: _onCameraMove,
+            // markers: _marker,
+            markers: Set<Marker>.of(<Marker>[
+              Marker(
+                  onTap: () {
+                    print('Tap');
+                  },
+                  draggable: true,
+                  markerId: MarkerId("marker"),
+                  position: LatLng(deviceLat.latitude, deviceLat.longitude),
+                  onDragEnd: ((newPosition) async {
+                    placemarks = await placemarkFromCoordinates(
+                        newPosition.latitude, newPosition.longitude);
+                    setState(() {
+                      location =
+                          GeoPoint(newPosition.latitude, newPosition.longitude);
+                      placemarks = placemarks;
+                    });
+                    // print(newPosition.latitude);
+                    // print(newPosition.longitude);
+                  }))
+            ]),
+            onCameraMove: ((_position) => _updatePosition(_position)),
           ),
-          Text(deviceLat.longitude.toString() +
-              ' ' +
-              deviceLat.latitude.toString()),
-          TextButton(
-              onPressed: () async {
-                userViewModelController.user.value.location =
-                    GeoPoint(deviceLat.latitude, deviceLat.longitude);
-                await userViewModelController
-                    .updateUserLocation(userViewModelController.userID,
-                        userViewModelController.user.value.location)
-                    .then((value) {
-                  Get.to(saveLocation());
-                });
-              },
-              child: Text('동네 설정하기')),
+          Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  // height: 50,
+                  color: Colors.yellow,
+                  child: Text(
+                      '${placemarks.first.street.toString()} ( ${location.longitude.toString().substring(0, 5)}, ${location.latitude.toString().substring(0, 4)} )'),
+                ),
+              ),
+              Expanded(flex: 15, child: SizedBox()),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  margin: EdgeInsets.all(15),
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        userViewModelController.user.value.location =
+                            GeoPoint(location.latitude, location.longitude);
+                        await userViewModelController
+                            .updateUserLocation(userViewModelController.userID,
+                                userViewModelController.user.value.location)
+                            .then((value) {
+                          Get.to(saveLocation());
+                        });
+                      },
+                      child: Text('동네 설정하기')),
+                ),
+              )
+            ],
+          ),
         ],
       )),
     );
